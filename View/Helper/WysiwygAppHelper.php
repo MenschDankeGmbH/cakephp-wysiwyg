ea8ac73b4ec98a1ed2b12320b1c09a5fac51c0e8
chris <christian.krueger@menschdanke.de>
2014-01-14 18:20:57 +0100

    Custom Buttons

--- View/Helper/WysiwygAppHelper.php
+++ View/Helper/WysiwygAppHelper.php
@@ -25,6 +25,7 @@ App::uses('AppHelper', 'View/Helper');
  * @property HtmlHelper $Html
  * @property JsHelper $Js
  */
+
 class WysiwygAppHelper extends AppHelper {
 
 /**
@@ -54,7 +55,7 @@ class WysiwygAppHelper extends AppHelper {
 	protected $_helperOptions = array();
 
 /**
- * Sets the $this->helper to the helper configured in the session.
+ * Sets the $this->helper to the helper configured in the session
  *
  * @param View $View The View this helper is being attached to.
  * @param array $settings Configuration settings for the helper.
@@ -68,7 +69,7 @@ class WysiwygAppHelper extends AppHelper {
 	}
 
 /**
- * Creates a wsyiwyg input field.
+ * Creates an fckeditor input field
  *
  * @param string $fieldName This should be "Modelname.fieldname"
  * @param array $options Each type of input takes different options.
@@ -76,18 +77,16 @@ class WysiwygAppHelper extends AppHelper {
  * @return string An HTML input element with Wysiwyg Js
  */
 	public function input($fieldName, $options = array(), $helperOptions = array()) {
-		$defaults = array();
-		if (isset($helperOptions['template'])) {
-			$defaults = (array)Configure::read('Wysiwyg.' . $helperOptions['template']);
-			unset($helperOptions['template']);
+		$model = false;
+		if (!empty($this->request->params['models'])) {
+			$model = key($this->request->params['models']) . '.';
 		}
-		$defaults = Set::merge($this->_helperOptions, $defaults);
-		$helperOptions = Set::merge($defaults, $helperOptions);
-		return $this->Form->input($fieldName, $options) . $this->_build($fieldName, $helperOptions);
+		$helperOptions = $this->_formatOptions($model, $helperOptions);
+		return $this->Form->input($fieldName, $options) . $this->_build($model . $fieldName, $helperOptions);
 	}
 
 /**
- * Creates a wsyiwyg textarea.
+ * Creates an fckeditor textarea
  *
  * @param string $fieldName This should be "Modelname.fieldname"
  * @param array $options Each type of input takes different options.
@@ -95,18 +94,16 @@ class WysiwygAppHelper extends AppHelper {
  * @return string An HTML textarea element with Wysiwyg Js
  */
 	public function textarea($fieldName, $options = array(), $helperOptions = array()) {
-		$defaults = array();
-		if (isset($helperOptions['template'])) {
-			$defaults = (array)Configure::read('Wysiwyg.' . $helperOptions['template']);
-			unset($helperOptions['template']);
+		$model = false;
+		if (!empty($this->request->params['models'])) {
+			$model = key($this->request->params['models']) . '.';
 		}
-		$defaults = Set::merge($this->_helperOptions, $defaults);
-		$helperOptions = Set::merge($defaults, $helperOptions);
-		return $this->Form->textarea($fieldName, $options) . $this->_build($fieldName, $helperOptions);
+		$helperOptions = $this->_formatOptions($model, $helperOptions);
+		return $this->Form->textarea($fieldName, $options) . $this->_build($model . $fieldName, $helperOptions);
 	}
 
 /**
- * Initializes the helper css and js for a given input field.
+ * Initialize the helper css and js for a given input field
  *
  * @param array $options array of css files, javascript files, and css text to enqueue
  * @return void
@@ -136,7 +133,7 @@ class WysiwygAppHelper extends AppHelper {
 	}
 
 /**
- * Returns a json string containing helper settings.
+ * Returns a json string containing helper settings
  *
  * @param array $options array of Wysiwyg editor settings
  * @return string json_encoded array of options
@@ -150,10 +147,81 @@ class WysiwygAppHelper extends AppHelper {
 			'_editor' => true,
 		);
 
-		return json_encode(array_diff_key(array_merge(
-			$defaults,
-			(array)$options
-		), $defaults));
+		$settings = array_diff_key(array_merge($defaults, (array)$options), $defaults);
+
+		$value_arr = array();
+		$replace_keys = array();
+		foreach ($settings as $key => &$value) {
+			if (strpos($value, 'function(') === 0) {
+				$value_arr[] = $value;
+				$value = '%' . $key . '%';
+				$replace_keys[] = '"' . $value . '"';
+			}
+		}
+		$json = json_encode($settings);
+		$json = str_replace($replace_keys, $value_arr, $json);
+		return $json;
+	}
+
+	/**
+	 * Modifies the option depending on given paremeter.
+	 * Different output depending on model.
+	 *
+	 * @param string $model The name of the current model
+	 * @param array $helperOptions Options from the view template
+	 * @return array
+	 */
+	protected function _formatOptions($model, $helperOptions = array()) {
+		$defaults = array();
+		$setup = array();
+		if (isset($helperOptions['template'])) {
+			$defaults = (array)Configure::read('Wysiwyg.' . $helperOptions['template']);
+			unset($helperOptions['template']);
+		}
+		$defaults = Set::merge($this->_helperOptions, $defaults);
+		if (empty($helperOptions['autoFields'])) {
+			return Set::merge($defaults, $helperOptions);
+		}
+		switch ($model) {
+			case 'Offer':
+				$setup = array('shop' => array('label' => 'Shopname', 'output' => '{SHOP}'),
+						'date' => array('label' => __('Current Date'), 'output' => '{DATE}'),
+						'start' => array('label' => __('Startdate'), 'output' => '{START}'),
+						'end' => array('label' => __('Enddate'), 'output' => '{END}'),
+						'worth' => array('label' => __('Worth'), 'output' => '{VALUE}'),
+					);
+				break;
+			case 'Shop':
+				$setup = array('shop' => array('label' => 'Shopname', 'output' => '{SHOP}'),
+						'date' => array('label' => __('Current Date'), 'output' => '{DATE}'),
+					);
+				break;
+			default:
+				$setup = array();
+				break;
+		}
+
+		if ($helperOptions['autoFields'] == 'select') {
+			$inputName = 'insertButton';
+			$defaults['setup'] = 'function(editor) { editor.addButton("' . $inputName . '", { text: "Autofields", type: "menubutton", icon: false, menu: [';
+			foreach ($setup as $key => $value) {
+				$defaults['setup'] .= '{text: "' . $value['label'] . '", label: "Select :", icon: false, onclick: function() { editor.insertContent("' . $value['output'] . '"); }},';
+			}
+			$defaults['setup'] .= ']});}';
+			$defaults['toolbar'] .= ' | '.$inputName;
+		} else {
+			$defaults['toolbar'] .= ' |';
+			$defaults['setup'] = 'function(editor) {';
+			foreach ($setup as $key => $value) {
+				$defaults['setup'] .= 'editor.addButton("insertButton' . ucfirst($key) . '", { text: "' . $value['label'] . '", label: "Select :", icon: false, onclick: function() { editor.insertContent("' . $value['output'] . '"); }});';
+				$defaults['toolbar'] .= ' insertButton' . ucfirst($key);
+			}
+			$defaults['setup'] .= '}';
+		}
+		unset($helperOptions['autoFields']);
+
+		$helperOptions = Set::merge($defaults, $helperOptions);
+		return $helperOptions;
 	}
 
 }
